176126a38a18c3d3fc495bbfb25ec389f5f76d50
Khanh <Khanh13Nguyen13@gmail.com>
2024-09-05 11:28:44 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<form>
+				<div>
+					<p>How much was the bill ?</p>
+					<input type="number" placeholder="0" />
+				</div>
+
+				<div>
+					<p>How did you like the service ?</p>
+					<select>
+						<option value={1}>Dissatisfied (0%)</option>
+						<option value={2}>It was okay (5%)</option>
+						<option value={3}>It was good (10%)</option>
+						<option value={3}>Absolutely amazing! (20%)</option>
+					</select>
+				</div>
+
+				<div>
+					<p>How did your friend like the service ?</p>
+					<select>
+						<option value={1}>Dissatisfied (0%)</option>
+						<option value={2}>It was okay (5%)</option>
+						<option value={3}>It was good (10%)</option>
+						<option value={3}>Absolutely amazing! (20%)</option>
+					</select>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
